478eaa578c5ccc2eee8c22e452f3aca3d844d5b9
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2022-04-06 10:39:00 -0300

    Informacao de Campos Vazios

--- app/src/main/java/br/mdan/alunoaprovadoreprovado/MainActivity.kt
+++ app/src/main/java/br/mdan/alunoaprovadoreprovado/MainActivity.kt
@@ -2,6 +2,7 @@ package br.mdan.alunoaprovadoreprovado
 
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -9,29 +10,57 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
 
-        val btnVerificar = Verificar
-        val resultado = Resultado
+        //val btnVerificar = Verificar
+        //val resultado = Resultado
 
-        btnVerificar.setOnClickListener{
-
-            val notaF = Integer.parseInt(NotaF.text.toString())
-            val notaS = Integer.parseInt(NotaS.text.toString())
-            val media = (notaF + notaS)/2
-            val faltas = Integer.parseInt(Faltas.text.toString())
-            val nome = Nome.text.toString()
-
-            if (media > 6 && faltas <= 10) {
-                resultado.setText(nome + "Aprovado" +
-                        "\nMédia: " + media +
-                        "\nQuantidade de Faltas: " + faltas )
-                resultado.setTextColor(Color.GREEN)
-            } else {
-                resultado.setText(nome + "Reprovado" +
-                        "\nMédia: " + media +
-                        "\nQuantidade de Faltas: " + faltas )
-                resultado.setTextColor(Color.RED)
+        private fun setListeners() {
+            Verificar.setOnClickListener {
+                mostrar(NotaF.text.toString(), NotaS.text.toString(), Faltas.text.toString())
             }
         }
-    }
+
+        private fun mostrar(
+            notaF: String,
+            notaS: String,
+            faltas: String,
+        ) {
+            //val notaF = Integer.parseInt(NotaF.text.toString())
+            //val notaS = Integer.parseInt(NotaS.text.toString())
+            //val media = (notaF + notaS) / 2
+            //val faltas = Integer.parseInt(Faltas.text.toString())
+            //val nome = Nome.text.toString()
+
+            val notaF = notaF.toFloatOrNull()
+            val notaS = notaS.toFloatOrNull()
+            val faltas = faltas.toFloatOrNull()
+            val nome = Nome.text.toString()
+
+            if (notaF != null && notaS != null && faltas != null && nome != null) {
+                val media = (notaF + notaS) / 2
+                if (media > 6 && faltas <= 10) {
+                    Resultado.setText(
+                        nome + " Aprovado" +
+                                "\nMédia: " + media +
+                                "\nQuantidade de Faltas: " + faltas
+                    )
+                    Resultado.setTextColor(Color.GREEN)
+                } else {
+                    Resultado.setText(
+                        nome + " Reprovado" +
+                                "\nMédia: " + media +
+                                "\nQuantidade de Faltas: " + faltas
+                    )
+                    Resultado.setTextColor(Color.RED)
+                }
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Insira os Valores Solicitados!",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
 }
